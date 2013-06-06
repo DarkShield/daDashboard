56da673dbdde27b522afb13fcdecca8f9852345e
Zach Jones <prozachj@gmail.com>
2013-06-05 23:40:22 -0500

    bringing over changes from today so we can build forward on the dashboard in this repo

--- app/server.js
+++ app/server.js
@@ -1,35 +1,21 @@
 var express = require('express');
-var domainRoutes = require('./routes/domainRoutes');
-var app = express();
 var mongoose = require('mongoose');
 
+var app = express();
+var Routes = require('./routes/router');
+
 //middleware order matters
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.session({ secret: 'SuperSecretKeyForNow' }));
 
 //routes
-app.get('/domains', domainRoutes.getDomains);
+app.get('/domains', Routes.domains);
+app.get('/login', Routes.loginpage);
 
-app.post('/domains/info', domainRoutes.drillDown);
-app.post('/domains/attacks', domainRoutes.getAttacks);
+app.post('/login', Routes.login);
+app.post('/domains/info', Routes.domains.info);
+app.post('/domains/attacks', Routes.domains.attacks);
 
-app.post('/dashboard', function (req, res) {
-	'use strict';
-	domainRoutes.getAuth(req, res);
-});
-
-app.post('/domains/info', function (req, res) {
-    'use strict';
-    domainRoutes.drillDown(req, res);
-});
-app.post('/domains/attacks', function (req, res) {
-   'use strict';
-   domainRoutes.getAttacks(req, res);
-});
-/*
-mongoose.connect('10.192.198.253', 'vicetest');
-app.listen(1337);
-*/
 module.exports = app;
