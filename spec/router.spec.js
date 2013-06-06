00246065f5913cccd7ba6a9ead680ecde852ed40
ProZachJ <prozachj@gmail.com>
2013-06-06 00:02:14 -0500

    change remote db to localhost for dev

--- spec/router.spec.js
+++ spec/router.spec.js
@@ -1,7 +1,7 @@
 var routes = require('../app/routes/router');
 var mongoose = require('mongoose');
 
-mongoose.connect('10.192.198.253', 'vicetest', function(err){
+mongoose.connect('localhost', 'vicetest', function(err){
   if (err) throw err;
   console.log('Successfully connected to mongo');
 });
