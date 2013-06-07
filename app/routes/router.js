1f917377a5de737cf4d8bf2f1023983fd1976b83
Zach Jones <prozachj@gmail.com>
2013-06-07 00:14:24 -0500

    export User from routes so that model functions can be stubbed

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,6 +1,8 @@
 var User = require('../model/user'),
     RequestStore = require('../../lib/requestSchema');
 
+exports.user = User;
+
 exports.loginpage = function loginpage (req, res) {
   res.sendfile('./public/html/login.html');
 }
@@ -9,7 +11,7 @@ exports.loginpage = function loginpage (req, res) {
 exports.login = function authenticate (req, res) {
   var username = req.body.username;
   var password = req.body.password;
-  var respond = function (err, user, reason) {
+  var respond = function respond (err, user, reason) {
     if (user !==null){
       req.session.user = user;
       res.sendfile('./routes/html/dashboard.html');
--- spec/router.spec.js
+++ spec/router.spec.js
@@ -50,6 +50,7 @@ describe('routes', function(){
             redirect: function(req, res){}
           };
       spyOn(res, 'sendfile', 'redirect');
+      spyOn(routes.user,'getAuthenticated').andCallThrough();
       runs(function(){
         routes.login(req, res);
       });
@@ -61,6 +62,10 @@ describe('routes', function(){
       runs(function() {
         expect(res.sendfile).toHaveBeenCalled();
         expect(res.sendfile).toHaveBeenCalledWith('./routes/html/dashboard.html');
+        expect(routes.user.getAuthenticated).toHaveBeenCalled();
+	expect(routes.user.getAuthenticated.calls[0].args[0]).toBe('mattjay');
+	expect(routes.user.getAuthenticated.calls[0].args[1]).toBe('mattjay');
+	expect(routes.user.getAuthenticated.calls[0].args[2].name).toBe('respond');	
       });
     });
 
