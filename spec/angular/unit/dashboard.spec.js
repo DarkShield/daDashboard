9d98edd200713678299ae6dbfb54c51c7b0caa3e
Zach Jones <prozachj@gmail.com>
2013-06-12 20:50:48 -0500

    Lots of karma love. Working dashboard.js Unit test. Not working midway stuff but files are here.

---
+++ spec/angular/unit/dashboard.spec.js
@@ -0,0 +1,26 @@
+describe('Unit: Testing Modules', function() {
+  describe('Dashboard  Module:', function() {
+    
+    beforeEach(function(){
+      var App = angular.mock.module('dashboard')
+    });
+
+      it('should be registered', function() {
+        expect(dashboard).not.toBe(null);
+      });
+    
+    describe("Dependencies", function(){
+      var deps;
+      var hasModule = function(m){
+      	return deps.indexOf(m) >= 0;
+      };
+      beforeEach(function(){
+        deps = App.value('appName').requires;
+      });
+
+      it('should have $stap.directives as a dependency', function(){
+        expect(hasModule('$strap.directives')).toBe(true);
+      });
+    });  
+  });
+});
