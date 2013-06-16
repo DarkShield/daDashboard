c8960160ec002aeab91cf787abf4407224959213
mattjay <matt@mattjay.com>
2013-06-16 22:39:48 +0000

    working test for select function in sidebarCtrl

--- spec/angular/unit/sidebarCtrl.spec.js
+++ spec/angular/unit/sidebarCtrl.spec.js
@@ -1,15 +1,33 @@
 //
 // spec/angular/unit/sidebarCtrl.spec.js
 //
+var domainStatic = { name: 'test.com', selected: '', requestData: {}};
+
 describe('Unit: Testing Controllers', function() {
   describe('Sidebar Controller:', function() {
     var ctrl, scope, service;
 
     beforeEach(angular.mock.module('App'));
     beforeEach(angular.mock.module('App.SidebarCtrl'));
-    beforeEach(angular.mock.inject(function($controller, $rootScope, domainService) {
+    
+    beforeEach(angular.mock.inject(function($controller, $rootScope, $httpBackend, domainService) {
       scope = $rootScope.$new();
+
       service = domainService;
+
+      httpBackend = $httpBackend;
+
+      domain = domainStatic;
+     
+      //Create spies for domainService functions 
+      spyOn(service, 'getDomains');//.andCallThrough();
+      spyOn(service, 'getRequestData');//.andCallThrough();
+      spyOn(service, 'getSelectedSite');//.andCallThrough();
+
+      //httpBackend.expectGET('/domains').respond(domain);
+      //httpBackend.expectPOST('/domain/info').respond(domain);
+      //httpBackend.expectPOST('/domain/attacks').respond(domain);
+
       ctrl = $controller('SidebarCtrl', {
         $scope: scope,
         domainService: service
@@ -27,6 +45,15 @@ describe('Unit: Testing Controllers', function() {
       expect(scope.test).not.toBe(undefined);
       expect(scope.sites).not.toBe(undefined);
       expect(scope.select).not.toBe(undefined);
+    });
+
+    it('should have a working select function', function() {
+      spyOn(scope, 'select').andCallThrough();
+      scope.select(domain);
+
+      expect(scope.select).toHaveBeenCalled();
+      expect(domain.selected).toBe('active');
+      expect(scope.getRequestData).toHaveBeenCalled();
     });
 
   });
