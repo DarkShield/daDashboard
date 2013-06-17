5d0e8dabdaeec37d8c550cf5997d8264ad21aefe
mattjay <matt@mattjay.com>
2013-06-17 00:44:03 +0000

    Cant figure out how to test the service functions with $httpBackend either for whatever reason.

--- spec/angular/unit/domainService.spec.js
+++ spec/angular/unit/domainService.spec.js
@@ -9,6 +9,9 @@ describe('Unit: Testing Services', function() {
     });*/
 
     beforeEach(angular.mock.module('App'));
+    beforeEach(inject(function($injector) {
+      $httpBackend = $injector.get('$httpBackend');
+    }));
 
     it('should contain a domainService service', inject(function(domainService) {
       expect(domainService).not.toBe(undefined);
@@ -18,6 +21,19 @@ describe('Unit: Testing Services', function() {
       expect($ds.getDomains).not.toBe(undefined);
       expect($ds.getRequestData).not.toBe(undefined);
       expect($ds.getSelectedSite).not.toBe(undefined);
+    }]));
+
+    it('should have a working getDomains function', inject(['domainService', function($ds) {
+      //TODO: WTF?! This should work. When I try to flush it says nothing queued up but there should
+      //be a GET to /domains queued up. 
+      spyOn($ds, 'getDomains').andCallThrough();
+      //$ds.getDomains();
+      $httpBackend.expectGET('/domains').respond([{name: 'test.com'}]);
+      expect($ds.domains).toBe(undefined);
+      //spyOn($ds, 'getDomains').andCallThrough();
+      //$httpBackend.flush();
+      
+      console.log($ds.domains);
     }]));
 
   });
