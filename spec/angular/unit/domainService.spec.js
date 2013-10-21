481df2b87054f9d5efe63ac3fa66d5f696e372e0
Zach Jones <zach.jones@whitehatsec.com>
2013-10-21 12:35:36 -0500

    refactored a much cleaner way of injecting the $httpBackend and domainService.

--- spec/angular/unit/domainService.spec.js
+++ spec/angular/unit/domainService.spec.js
@@ -1,27 +1,31 @@
 describe('Domain Service:', function() {
+  var dS = null;
+  beforeEach(function(){
+    angular.mock.module('App');
+    angular.mock.module('App.domainService');
+    inject(function($injector, domainService) {
+      $httpBackend = $injector.get('$httpBackend');
+      $httpBackend.when('GET', '/domains').respond([{name: 'test.com'}]);
+      dS = domainService;
+    });
+    spyOn(dS, 'getDomains').andCallThrough();
+  });
 
-  beforeEach(angular.mock.module('App'));
-  beforeEach(inject(function($injector) {
-    $httpBackend = $injector.get('$httpBackend');
-    $httpBackend.when('GET', '/domains').respond([{name: 'test.com'}]);
-  }));
+  it('should contain a domainService service', function() {
+    expect(dS).not.toBe(undefined);
+  });
 
-  it('should contain a domainService service', inject(function(domainService) {
-    expect(domainService).not.toBe(undefined);
-  }));
+  it('should have a domainService service with all its functions', function() {
+    expect(dS.getDomains).not.toBe(undefined);
+    expect(dS.getRequestData).not.toBe(undefined);
+    expect(dS.getSelectedSite).not.toBe(undefined);
+  });
 
-  it('should have a domainService service with all its functions', inject(['domainService', function($ds) {
-    expect($ds.getDomains).not.toBe(undefined);
-    expect($ds.getRequestData).not.toBe(undefined);
-    expect($ds.getSelectedSite).not.toBe(undefined);
-  }]));
-
-  it('should have a working getDomains function', inject(['domainService', function($ds) {
-    //TODO: WTF?! This should work. When I try to flush it says nothing queued up but there should
-    //be a GET to /domains queued up.
+  it('should have a working getDomains function', function() {
     $httpBackend.expectGET('/domains');
-    spyOn($ds, 'getDomains').andCallThrough();
-    //$httpBackend.flush();
-    console.log($ds.doms);
-  }]));
+    expect(dS.getDomains).not.toHaveBeenCalled();
+    dS.getDomains();
+    expect(dS.getDomains).toHaveBeenCalled();
+    $httpBackend.flush();
+  });
 });
