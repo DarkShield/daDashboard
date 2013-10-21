3f6ade3acffa93697b4debed537eefbfc98cf33f
Zach Jones <zach.jones@whitehatsec.com>
2013-10-21 12:41:55 -0500

    refactored grouped method test into seperate it blocks

--- spec/angular/unit/domainService.spec.js
+++ spec/angular/unit/domainService.spec.js
@@ -11,14 +11,23 @@ describe('Domain Service:', function() {
     spyOn(dS, 'getDomains').andCallThrough();
   });
 
-  it('should contain a domainService service', function() {
+  it('should be registered', function() {
     expect(dS).not.toBe(undefined);
   });
 
-  it('should have a domainService service with all its functions', function() {
+  it('should have a getDomains method', function(){
     expect(dS.getDomains).not.toBe(undefined);
+    expect(typeof dS.getDomains).toBe('function');
+  });
+
+  it('should have a getRequestData method', function() {
     expect(dS.getRequestData).not.toBe(undefined);
+    expect(typeof dS.getRequestData).toBe('function');
+  });
+
+  it('should have a getSelected site method',function(){
     expect(dS.getSelectedSite).not.toBe(undefined);
+    expect(typeof dS.getSelectedSite).toBe('function');
   });
 
   it('should have a working getDomains function', function() {
