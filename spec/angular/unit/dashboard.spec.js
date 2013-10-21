e55ed8a2adb33272a6222b8d64e2e567bbb6d32f
Zach Jones <zach.jones@whitehatsec.com>
2013-10-21 10:00:56 -0500

    remove uneeded describe block and fix indention

--- spec/angular/unit/dashboard.spec.js
+++ spec/angular/unit/dashboard.spec.js
@@ -1,42 +1,31 @@
-describe('Unit: Testing Modules', function() {
-  describe('Dashboard  Module:', function() {
-    
-    /*beforeEach(function(){
-      var App = angular.mock.module('dashboard')
+describe('Dashboard  Module:', function() {
+
+  beforeEach(angular.mock.module('App'));
+
+  it('should be registered', function() {
+    expect(App).not.toBe(null);
+  });
+
+  describe("Dependencies", function(){
+    var deps;
+    var hasModule = function(m){
+      return deps.indexOf(m) >= 0;
+     };
+     beforeEach(function(){
+      deps = App.value('appName').requires;
+     });
+
+    it('should have App.drilldownCtrl as a dependency', function(){
+      expect(hasModule('App.drilldownCtrl')).toBe(true);
     });
 
-      it('should be registered', function() {
-        expect(dashboard).not.toBe(null);
-      });
-    */
-
-    beforeEach(angular.mock.module('App'));
-
-    it('should be registered', function() {
-      expect(App).not.toBe(null);
+    it('should have App.SidebarCtrl as a dependency', function(){
+      expect(hasModule('App.SidebarCtrl')).toBe(true);
     });
 
-    describe("Dependencies", function(){
-      var deps;
-      var hasModule = function(m){
-      	return deps.indexOf(m) >= 0;
-      };
-      beforeEach(function(){
-        deps = App.value('appName').requires;
-      });
-
-      it('should have App.drilldownCtrl as a dependency', function(){
-        expect(hasModule('App.drilldownCtrl')).toBe(true);
-      });
-
-      it('should have App.SidebarCtrl as a dependency', function(){
-        expect(hasModule('App.SidebarCtrl')).toBe(true);
-      });
-
-      it('should have App.domainService as a dependency', function(){
-        expect(hasModule('App.domainService')).toBe(true);
-      });
-
+    it('should have App.domainService as a dependency', function(){
+      expect(hasModule('App.domainService')).toBe(true);
     });
   });
 });
+
