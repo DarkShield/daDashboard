484d2c19c05e46653eb4093a121804c05f094834
Zach Jones <zach.jones@whitehatsec.com>
2013-10-21 10:03:34 -0500

    remove uneeded describe block

--- spec/angular/unit/drilldownCtrl.spec.js
+++ spec/angular/unit/drilldownCtrl.spec.js
@@ -1,52 +1,48 @@
-//
-// spec/angular/unit/drilldownCtrl.spec.js
-//
+
 var domainStatic = { name: 'test.com', selected: '', requestData: {}};
 
-describe('Unit: Testing Controllers', function() {
-  describe('Drilldown Controller:', function() {
-    var ctrl, scope, service;
+describe('Drilldown Controller:', function() {
+  var ctrl, scope, service;
 
-    beforeEach(angular.mock.module('App'));
-    beforeEach(angular.mock.module('App.drilldownCtrl'));
+  beforeEach(angular.mock.module('App'));
+  beforeEach(angular.mock.module('App.drilldownCtrl'));
 
-      beforeEach(angular.mock.inject(function($controller, $rootScope, $httpBackend, domainService) {
-      scope = $rootScope.$new();
+  beforeEach(angular.mock.inject(function($controller, $rootScope, $httpBackend, domainService) {
+    scope = $rootScope.$new();
 
-      service = domainService;
+    service = domainService;
 
-      httpBackend = $httpBackend;
+    httpBackend = $httpBackend;
 
-      domain = domainStatic;
+    domain = domainStatic;
 
-      //spyOn(service, 'getDomains');
-      //spyOn(service, 'getRequestData');
-      //spyOn(service, 'getSelectedSite');//.andCallThrough();
+    //spyOn(service, 'getDomains');
+    //spyOn(service, 'getRequestData');
+    //spyOn(service, 'getSelectedSite');//.andCallThrough();
 
-      ctrl = $controller('drilldownCtrl', {
-        $scope: scope,
-        domainService: service
-      });
-    }));
-
-    it('should have a drilldownCtrl controller', function() {
-      expect(ctrl).not.toBe(undefined);
+    ctrl = $controller('drilldownCtrl', {
+      $scope: scope,
+      domainService: service
     });
+  }));
 
-    xit('should have all the necessary parameters', function() {
-      expect(scope.sort).not.toBe(undefined);
-      expect(scope.filterby).not.toBe(undefined);
-      expect(scope.details).not.toBe(undefined);
-      expect(scope.attacks).not.toBe(undefined);
-      expect(scope.displayFilter).not.toBe(undefined);
-    });
+  it('should have a drilldownCtrl controller', function() {
+    expect(ctrl).not.toBe(undefined);
+  });
 
-    xit('should have a working details function', function() {
-      spyOn(scope, 'details').andCallThrough();
+  xit('should have all the necessary parameters', function() {
+    expect(scope.sort).not.toBe(undefined);
+    expect(scope.filterby).not.toBe(undefined);
+    expect(scope.details).not.toBe(undefined);
+    expect(scope.attacks).not.toBe(undefined);
+    expect(scope.displayFilter).not.toBe(undefined);
+  });
 
-      //TODO: why can't I get this to pass?!
-      //expect(service.getSelectedSite).toHaveBeenCalled();
+  it('should have a working details function', function() {
+    spyOn(scope, 'details').andCallThrough();
 
-    });
+    //TODO: why can't I get this to pass?!
+    //expect(service.getSelectedSite).toHaveBeenCalled();
+
   });
 });
