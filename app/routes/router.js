a10112d086ec59c1b87521363b3d24e4698db3cb
Matt <matt@mattjay.com>
2013-10-29 15:27:36 -0500

    fixing loop part deux

--- app/routes/router.js
+++ app/routes/router.js
@@ -127,9 +127,8 @@ exports.traffic = function getRange (req, res) {
   var sitesArray = [];
   for (var site in req.session.user.sites) {
     if (req.session.user.sites.hasOwnProperty(site)) {
-      console.log(site);
-      console.log(site.name);
-      sitesArray.push(site.name);
+      console.log(req.session.user.sites[site]);
+      sitesArray.push(req.session.user.sites[site].name);
     }
     else console.log('doesnt have prop');
   }
