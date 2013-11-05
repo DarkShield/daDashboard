95d044a00a7d509488c1d7fa21191c9ab84c17b9
mattjay <matt@mattjay.com>
2013-11-05 04:31:20 +0000

    change db to prod db

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 var app = require('./server'),
     mongoose = require('mongoose');
 
-mongoose.connect('localhost', 'vicetest');
-//mongoose.connect('10.192.198.253', 'vicetest');
+//mongoose.connect('localhost', 'vicetest');
+mongoose.connect('10.192.198.253', 'vicetest');
 app.listen(1337);
