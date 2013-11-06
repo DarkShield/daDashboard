7ff884f8dea6e6072a727d369b8d8ca86f917757
Zach Jones <zach.jones@whitehatsec.com>
2013-11-05 21:38:41 -0600

    environment configuration using NODE_ENV so that we don't have to keep changing from dev to prod.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,20 @@
 var app = require('./server'),
     mongoose = require('mongoose');
 
-mongoose.connect('localhost', 'vicetest');
-//mongoose.connect('10.192.198.253', 'vicetest');
+//To set environment run
+// $export NODE_ENV=development
+//or
+// $export NODE_ENV=production
+
+if (app.get('env') === 'development') {
+  app.set('db uri', 'localhost');
+  app.set('db name', 'vicetest');
+}
+
+if (app.get('env') === 'production' ) {
+  app.set('db uri', '10.192.198.253');
+  app.set('db name', 'viceprod');
+}
+
+mongoose.connect(app.get('db uri'), app.get('db name'));
 app.listen(1337);
