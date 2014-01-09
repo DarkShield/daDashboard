b7072a02744db27dfe79dde4b0e737d00d078030
Matt <matt@mattjay.com>
2014-01-09 12:39:06 -0600

    fixed all server tests for the routes in the express file.

--- spec/node/unit/server.spec.js
+++ spec/node/unit/server.spec.js
@@ -20,7 +20,7 @@ describe("app", function(){
 
   it("should have a static route configured", function(){
     expect(typeof(app.stack[2].handle)).toBe('function');
-    expect(app.stack[2].handle.name).toBe('static');
+    expect(app.stack[2].handle.name).toBe('staticMiddleware');
   });
 
   it("should have bodyparser endabled", function(){
@@ -37,29 +37,71 @@ describe("app", function(){
     expect(routes).not.toBe(0);
   });
 
-  it("should have a GET route to  /domains that calls the getDomains function", function(){
+  it("should have a GET route to /domains that calls the getDomains function", function(){
     expect(app.routes.get[0].path).toBe('/domains');
-    expect(app.routes.get[0].callbacks[0].name).toBe('getDomains');
+    expect(app.routes.get[0].callbacks[1].name).toBe('getDomains');
   });
   
-  it("should have a GET route to  /login that calls the loginpage function", function(){
+  it("should have a GET route to /login that calls the loginpage function", function(){
     expect(app.routes.get[1].path).toBe('/login');
     expect(app.routes.get[1].callbacks[0].name).toBe('loginpage');
   });
 
+  it("should have a GET route to /signup that calls the signuppage function", function(){
+    expect(app.routes.get[2].path).toBe('/signup');
+    expect(app.routes.get[2].callbacks[0].name).toBe('signuppage');
+  });
+
+  it("should have a GET route to / that calls the homePage function if authenticated", function(){
+    expect(app.routes.get[3].path).toBe('/');
+    expect(app.routes.get[3].callbacks[0].name).toBe('loadUser');
+    expect(app.routes.get[3].callbacks[1].name).toBe('homePage');
+  });
+
+  it("should have a GET route to /logout that calls the logout function if authenticated", function(){
+    expect(app.routes.get[4].path).toBe('/logout');
+    expect(app.routes.get[4].callbacks[0].name).toBe('loadUser');
+    expect(app.routes.get[4].callbacks[1].name).toBe('logout');
+  });
+
+  it("should have a POST route to /signup that calls the authenticate function", function(){
+    expect(app.routes.post[0].path).toBe('/signup');
+    expect(app.routes.post[0].callbacks[0].name).toBe('addAccount');
+  });
+
   it("should have a POST route to /login that calls the authenticate function", function(){
-    expect(app.routes.post[0].path).toBe('/login');
-    expect(app.routes.post[0].callbacks[0].name).toBe('authenticate');
+    expect(app.routes.post[1].path).toBe('/login');
+    expect(app.routes.post[1].callbacks[0].name).toBe('authenticate');
   });
 
-  it("should have a POST route to /domains/info that calls the getDomainData function", function(){
-    expect(app.routes.post[1].path).toBe('/domains/info');
-    expect(app.routes.post[1].callbacks[0].name).toBe('getDomainData');
+  it("should have a POST route to /domains/info that calls the getDomainData function if authenticated", function(){
+    expect(app.routes.post[2].path).toBe('/domains/info');
+    expect(app.routes.post[2].callbacks[0].name).toBe('loadUser');
+    expect(app.routes.post[2].callbacks[1].name).toBe('getDomainData');
   });
 
-  it("should have a POST route to /domains/attacks that calls the getDomainAttacks function", function(){
-    expect(app.routes.post[2].path).toBe('/domains/attacks');
-    expect(app.routes.post[2].callbacks[0].name).toBe('getDomainAttacks');
+  it("should have a POST route to /domains/attacks that calls the getDomainAttacks function if authenticated", function(){
+    expect(app.routes.post[3].path).toBe('/domains/attacks');
+    expect(app.routes.post[3].callbacks[0].name).toBe('loadUser');
+    expect(app.routes.post[3].callbacks[1].name).toBe('getDomainAttacks');
+  });
+
+  it("should have a POST route to /domains/info/lastday that calls the getLastDay function if authenticated", function(){
+    expect(app.routes.post[4].path).toBe('/domains/info/lastday');
+    expect(app.routes.post[4].callbacks[0].name).toBe('loadUser');
+    expect(app.routes.post[4].callbacks[1].name).toBe('getLastDay');
+  });
+
+  it("should have a POST route to /traffic that calls the getRange function if authenticated", function(){
+    expect(app.routes.post[5].path).toBe('/traffic');
+    expect(app.routes.post[5].callbacks[0].name).toBe('loadUser');
+    expect(app.routes.post[5].callbacks[1].name).toBe('getRange');
+  });
+
+  it("should have a POST route to /toggle/attack that calls the toggleAttack function if authenticated", function(){
+    expect(app.routes.post[6].path).toBe('/toggle/attack');
+    expect(app.routes.post[6].callbacks[0].name).toBe('loadUser');
+    expect(app.routes.post[6].callbacks[1].name).toBe('toggleAttack');
   });
 
 
