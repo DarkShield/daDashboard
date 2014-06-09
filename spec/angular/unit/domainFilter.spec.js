be5906f5ee3932074616f08bbde49ff73a91e4b9
Matt <matt@mattjay.com>
2014-06-09 17:26:38 -0500

    skeleton tests based on filter test docs

--- spec/angular/unit/domainFilter.spec.js
+++ spec/angular/unit/domainFilter.spec.js
@@ -1,6 +1,14 @@
 /**
  * Created by mattjohansen on 6/9/14.
  */
-describe('domainFilter', function() {
-  
-})
+describe('Filters', function() {
+
+  beforeEach(module('App.Filters'));
+
+  describe('domainFilter', function() {
+    it('should filter based on domain', inject(function(domainFilter) {
+
+    }));
+  });
+
+});
